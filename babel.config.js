78c777e71061d894744814aa100b8805021f6c53
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2021-04-09 10:47:53 -0300

    first commit

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+const { runtime, AutomaticPrefetchPlugin } = require("webpack");
+
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    ['@babel/preset-react', {
+      runtime: 'automatic'
+    }]
+  ]
+}
---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import style from './styles/global.css'
+
+export function App() {
+ 
+  return <h1>Oi Senhor Jesus</h1>
+}
