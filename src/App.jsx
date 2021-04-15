cdfc8c766d19b623c2e4e65cf5916ab23f3f747e
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2021-04-15 15:41:33 -0300

    listing stylizattion

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import style from './styles/global.scss'
 import {RepositoryList} from "./components/RepositoryList"
-import { Counter } from './components/Counter'
 
 export function App() {
  
   return (
-    <>
-      <RepositoryList/>
-      <Counter/>
-    </>
+    <RepositoryList/>
   ) 
 }
