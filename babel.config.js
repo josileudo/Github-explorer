dca928c821bfd70e57cd11d637a2e183bf5b005d
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2021-04-27 16:20:27 -0300

    TypeScript in ReactJS

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@ const { runtime, AutomaticPrefetchPlugin } = require("webpack");
 module.exports = {
   presets: [
     '@babel/preset-env',
+    '@babel/preset-typescript',
     ['@babel/preset-react', {
       runtime: 'automatic'
     }]
