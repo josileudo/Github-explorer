40b2adec7e2a04cfef5b96dde6779f3f3416d8a4
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2021-04-27 16:34:03 -0300

    typescript in react

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const isDevelopment = process.env.NODE_ENV !== 'production'
 module.exports= {
   mode: isDevelopment ? 'development' : 'production',
   devtool: isDevelopment ? "eval-source-map" : 'source-map',
-  entry: path.resolve(__dirname, 'src', 'index.jsx'), 
+  entry: path.resolve(__dirname, 'src', 'index.tsx'), 
   output: { 
     path: path.resolve (__dirname, 'dist'),
     filename: 'bundle.js',
@@ -17,7 +17,7 @@ module.exports= {
     hot:true,
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
   },
   plugins:[
     isDevelopment && new ReactRefreshWebpackPLugin(),
@@ -28,7 +28,7 @@ module.exports= {
   module: { 
     rules:[ 
       {
-        test: /\.jsx$/, 
+        test: /\.(j|t)sx$/, 
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
